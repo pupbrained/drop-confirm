3d68e36cf8637ab401d3f192e2b756b2df7bdee7
Mars <mars@pupbrained.xyz>
2025-04-03 16:14:56 -0400

    ok this should be perfect now

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 @file:Suppress("PropertyName")
 
+import io.github.klahap.dotenv.DotEnvBuilder
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 val group: String by project
@@ -14,7 +15,7 @@ plugins {
   id("net.neoforged.moddev") version "2.0.80" apply false
   id("io.github.pacifistmc.forgix") version "1.2.9"
   id("me.modmuss50.mod-publish-plugin") version "0.8.4"
-  id("co.uzzu.dotenv.gradle") version "4.0.0"
+  id("io.github.klahap.dotenv") version "1.1.3"
   kotlin("jvm")
 }
 
@@ -40,6 +41,15 @@ tasks.named("mergeJars") {
 }
 
 project.afterEvaluate {
+  val envFilePath = rootDir.resolve(".env")
+
+  val envVars = DotEnvBuilder.dotEnv {
+    addSystemEnv()
+
+    if (envFilePath.exists() && envFilePath.isFile)
+      addFile(envFilePath.absolutePath)
+  }
+
   val forgixExtension = extensions.getByType(io.github.pacifistmc.forgix.plugin.ForgixMergeExtension::class.java)
 
   val mergedJarFileProvider = project.layout.projectDirectory.file(
@@ -74,7 +84,7 @@ project.afterEvaluate {
     """.trimIndent()
 
     github("github") {
-      accessToken.set(env.GITHUB_TOKEN.orNull())
+      accessToken.set(envVars["GITHUB_TOKEN"])
       repository.set("pupbrained/drop-confirm")
       commitish.set(minecraft_version)
       tagName.set("v$mod_version")
@@ -85,7 +95,7 @@ project.afterEvaluate {
     }
 
     curseforge("curseforge") {
-      accessToken.set(env.CURSEFORGE_TOKEN.orNull())
+      accessToken.set(envVars["CURSEFORGE_TOKEN"])
       projectId.set("881314")
       minecraftVersions.addAll(supportedVersionsList)
       modLoaders.add("fabric")
@@ -100,7 +110,7 @@ project.afterEvaluate {
     }
 
     modrinth("modrinth") {
-      accessToken.set(env.MODRINTH_TOKEN.orNull())
+      accessToken.set(envVars["MODRINTH_TOKEN"])
       projectId.set("I45rjF2F")
       minecraftVersions.addAll(supportedVersionsList)
       modLoaders.add("fabric")
