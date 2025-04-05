88bf19c33bbacd907bae4e7ca946bfcfa2b27523
Mars <mars@pupbrained.xyz>
2025-04-05 00:13:49 -0400

    ok i have no fucking idea what im doing

--- buildSrc/src/main/kotlin/build-extensions.kt
+++ buildSrc/src/main/kotlin/build-extensions.kt
@@ -1,6 +1,5 @@
 import dev.kikugie.stonecutter.build.StonecutterBuild
 import org.gradle.api.Project
-import org.gradle.language.jvm.tasks.ProcessResources
 
 /**
  * Retrieves a property value from the project based on the provided key.
@@ -25,57 +24,6 @@ fun Project.versionProp(key: String): String {
     project.prop(specificKey) != null -> project.prop(specificKey)!!
     project.prop(wildcardKey) != null -> project.prop(wildcardKey)!!
     else -> throw IllegalArgumentException("Missing '$specificKey' or '$wildcardKey'")
-  }
-}
-
-/**
- * Retrieves a version-specific or wildcard property key from the project's properties.
- *
- * @param key the specific key to identify the property related to the version or a wildcard.
- * @return the value of the property as a string if found, or null if the property is missing.
- */
-fun Project.versionPropOrNull(key: String): String? {
-  val specificKey = "vers.${stonecutter(project).current.version.replace(".", "_")}.$key"
-  val wildcardKey = "vers.*.$key"
-
-  return when {
-    project.prop(specificKey) != null -> project.prop(specificKey)!!
-    project.prop(wildcardKey) != null -> project.prop(wildcardKey)!!
-    else -> null
-  }
-}
-
-/**
- * Applies a set of properties to a `ProcessResources` task, based on the project's configuration
- * and additional resource files.
- *
- * @param project the Gradle project that contains the configuration and properties.
- * @param files an iterable collection of file paths to which the properties will be applied.
- */
-fun ProcessResources.applyProperties(project: Project, files: Iterable<String>) {
-  val props = mutableMapOf(
-    "mod_version" to project.prop("mod.version"),
-    "mod_group" to project.prop("mod.group"),
-    "mod_id" to project.prop("mod.id"),
-
-    "mod_name" to project.prop("mod.name"),
-    "mod_description" to project.prop("mod.description"),
-    "mod_license" to project.prop("mod.license"),
-
-    "minecraft_version" to stonecutter(project).current.version,
-  )
-
-  fun addNullable(name: String, value: String?) {
-    if (value != null) props[name] = value
-  }
-
-  addNullable("fabric_loader_version", project.versionPropOrNull("fabric_loader"))
-  addNullable("forge_loader_version", project.versionPropOrNull("forge_loader"))
-  addNullable("neoforge_loader_version", project.versionPropOrNull("neoforge_loader"))
-
-  inputs.properties(props)
-  filesMatching(files) {
-    expand(props)
   }
 }
 
