be48c772ca7be048889d724b8e0d849e6caab9d2
Mars <mars@pupbrained.xyz>
2025-04-08 20:09:53 -0400

    holy shid it works

--- src/main/kotlin/xyz/pupbrained/drop_confirm/loaders/forge/ForgeEntrypoint.kt
+++ src/main/kotlin/xyz/pupbrained/drop_confirm/loaders/forge/ForgeEntrypoint.kt
@@ -9,9 +9,6 @@ import net.minecraftforge.client.event.RegisterKeyMappingsEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.loading.FMLEnvironment
-import org.spongepowered.asm.launch.MixinBootstrap
-import org.spongepowered.asm.mixin.MixinEnvironment
-import org.spongepowered.asm.mixin.Mixins
 import thedarkcolour.kotlinforforge.forge.FORGE_BUS
 import thedarkcolour.kotlinforforge.forge.LOADING_CONTEXT
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
@@ -24,10 +21,6 @@ import xyz.pupbrained.drop_confirm.config.DropConfirmConfigScreen
 @Mod("drop_confirm")
 class ForgeEntrypoint {
   init {
-    MixinBootstrap.init()
-    Mixins.addConfiguration("drop_confirm.mixins.json")
-    MixinEnvironment.getCurrentEnvironment().obfuscationContext = "searge"
-
     MOD_BUS.addListener { event: RegisterKeyMappingsEvent -> event.register(TOGGLE_KEY) }
 
     LOADING_CONTEXT.registerExtensionPoint(ConfigScreenFactory::class.java) {
